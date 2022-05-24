ffb5ff353877580edc9b5d3a189de28249278135
shmodarresi <sh.modarresy@gmail.com>
2022-05-24 16:33:23 +0200

    feat(taquito): use taquito to validate the address

--- veri.admin/src/App.tsx
+++ veri.admin/src/App.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Grow, Typography } from "@mui/material";
 import QrReader from "react-qr-reader";
 import axios from "axios";
 import { Loading } from "./Loading";
-
-const Logo = styled.img`
-  margin:  2rem auto;
-  width: 13rem;
-  display: block;
-`;
+import { TransitionGroup } from "react-transition-group";
+import { validateAddress } from "@taquito/utils";
 
 const ScanContainer = styled(Box)`
   width: 100%;
   padding: 2rem 0;
   margin: auto;
-  max-width: 400px;
+  max-width: 600px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  .MuiBox-root {
+    width: 100%;
+    height: 100%;
+  }
   div {
     box-shadow: rgba(255, 255, 255, 0.5) 0px 0px 0px 5px inset !important;
   }
@@ -23,40 +26,74 @@ const ScanContainer = styled(Box)`
 
 const MainContainer = styled(Container)`
   padding: 2rem;
+  border-box: box-sizing;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
   .error {
     color: #d32f2f;
   }
 `;
 
+const Scanner = styled(QrReader)`
+  & > * {
+    height: 100%;
+    padding-top: 0 !important;
+  }
+`;
+
+const Logo = styled.img`
+  margin:  2rem auto;
+  width: 13rem;
+  display: block;
+`;
+
 const App = () => {
   const [loading, setLoading] = React.useState(false);
+  const [msgLoaded, setMsgLoaded] = React.useState(false);
+  const [scannerLoaded, setScannerLoaded] = React.useState(true);
   const [message, setMessage] = React.useState("");
   const [messageType, setMessageType] = React.useState("");
+
+  const handleTransitions = (msgData: boolean = false) => {
+    setMsgLoaded(msgData);
+    setScannerLoaded(!msgData);
+  };
 
   const handleScan = (data: any) => {
     if (data) {
       const scanData = data.substring(data.lastIndexOf("/") + 1);
-      setLoading(true);
-      try {
-        axios
-          .get(`https://veri.tzconnect.berlin/dyn/add.cgi?key=${scanData}`)
-          .then((res) => {
-            setMessage("Successfully submited the code.");
-            setMessageType("succeed");
-          })
-          .catch((err) => {
-            setMessage("Something went wrong");
-            setMessageType("error");
-          })
-          .finally(() => refreshPage());
-      } catch (error) {
-        console.log(error);
+      const validation = validateAddress(scanData);
+      if( validation === 3) {
+        try {
+          handleTransitions(true);
+          axios
+            .get(`https://veri.tzconnect.berlin/dyn/add.cgi?key=${scanData}`)
+            .then((res) => {
+              setMessage("Successfully submited the code.");
+              setMessageType("succeed");
+            })
+            .catch((err) => {
+              setMessage("Something went wrong");
+              setMessageType("error");
+            })
+            .finally(() => refreshPage());
+        } catch (error) {
+          console.log(error);
+        }
       }
-      setLoading(false);
+      else{
+        handleTransitions(true);
+        setMessage("Wallet address is not valid");
+        setMessageType("error");
+      }
+
+     
     }
   };
 
   const handleError = (err: any) => {
+    handleTransitions(true);
     setMessage("Unable to scan the code");
     setMessageType("error");
     console.error(err);
@@ -65,38 +102,37 @@ const App = () => {
 
   const refreshPage = () => {
     setTimeout(() => {
+      handleTransitions(false);
       setMessage("");
       setMessageType("");
     }, 5000);
   };
 
   return (
-    <div className="App">
-      <MainContainer>
-        <Logo src="./Logo.svg" />
-        {loading && <Loading />}
-        {message.length > 0 ? (
-          <Typography
-            my={8}
-            align="center"
-            variant="h5"
-            className={messageType}
-          >
-            {message}
-          </Typography>
-        ) : (
-          <ScanContainer>
-            <QrReader
+    <MainContainer>
+      <Logo src="./Logo.svg" />
+      {loading && <Loading />}
+      <ScanContainer>
+        <TransitionGroup>
+          <Grow in={msgLoaded}>
+            <Typography align="center" variant="h5" className={messageType}>
+              {message}
+            </Typography>
+          </Grow>
+        </TransitionGroup>
+        <Grow in={scannerLoaded}>
+          <Box>
+            <Scanner
               delay={300}
               onError={handleError}
               onScan={handleScan}
               onImageLoad={() => setLoading(true)}
-              style={{ width: "100%" }}
+              style={{ width: "100%", height: "100%" }}
             />
-          </ScanContainer>
-        )}
-      </MainContainer>
-    </div>
+          </Box>
+        </Grow>
+      </ScanContainer>
+    </MainContainer>
   );
 };
 
