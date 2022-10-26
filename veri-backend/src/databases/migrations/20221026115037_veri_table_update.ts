eed1f7f7d14af7206c42bade486a1c26c2f5bd3f
Nazmus Sakib <ns.sanim@gmail.com>
2022-10-26 14:09:25 +0200

    feat(migration): added new db migration to reflect new data types
    BREAKING CHANGE: Do the new migration using 'yarn migrate'

---
+++ veri-backend/src/databases/migrations/20221026115037_veri_table_update.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.table('veris', (table) => {
+    table.string('live_distribution_password', 64);
+    table.string('status', 10);
+    table.specificType('recipients', 'text ARRAY');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table('veris', (table) => {
+    table.dropColumn('live_distribution_password');
+    table.dropColumn('status');
+    table.dropColumn('recipients');
+  });
+}
