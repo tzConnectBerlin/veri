051e5a19d01c95d6604df2cc44c73de83bae4708
shmodarresi <sh.modarresy@gmail.com>
2022-11-17 18:49:07 +0100

    chore(postd data): use FormData for sending data to the server

--- veri-frontend/src/Global.ts
+++ veri-frontend/src/Global.ts
@@ -2,4 +2,4 @@ export const ADMIN_URL = '/admin';
 export const BASE_URL =
   process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
 export const SUPPORTED_FORMATS = '.JPG, .JPEC, .PNG, .GIF';
-export const DIMENTION_SIZE = 10000;
+export const DIMENTION_SIZE = 1000;
--- veri-frontend/src/utils/veri.ts
+++ veri-frontend/src/utils/veri.ts
@@ -1,21 +1,41 @@
 import { VeriFormValues } from '../types/veris';
 
 export const MapVeriToServerValue = (veri: VeriFormValues) => {
-  console.log(veri);
-  return {
-    event_name: veri.eventName,
-    event_description: veri.description,
-    event_contact_email: veri.organizerEmail,
-    event_type: veri.eventDuration,
-    event_start_date: new Date(),
-    event_end_date: new Date(),
-    artwork_name: veri.artworkFile?.name,
-    artwork_description: veri.description,
-    artwork_file: veri.artworkFile,
-    live_distribution: veri.distributionMethod === 'QR-code' ? true : false,
-    live_distribution_url: 'test',
-    live_distribution_password: 'test',
-    status: veri.status,
-    recipients: veri.recipients,
-  };
+  const formData = new FormData();
+
+  formData.append('event_name', veri.eventName);
+  formData.append('event_description', veri.description ?? '');
+  formData.append('event_contact_email', veri.organizerEmail);
+  formData.append('event_type', veri.eventDuration?.toString() ?? '');
+  formData.append('event_start_date', new Date().toISOString());
+  formData.append('event_end_date', new Date().toISOString());
+  formData.append('artwork_name', veri.artworkFile?.name ?? '');
+  formData.append('artwork_description', veri.description ?? '');
+  formData.append('artwork_file', veri.artworkFile ?? '');
+  formData.append(
+    'live_distribution',
+    (veri.distributionMethod === 'QR-code' ? true : false).toString(),
+  );
+  formData.append('live_distribution_url', '/test');
+  formData.append('live_distribution_password', 'test');
+  formData.append('status', veri.status);
+  formData.append('recipients', veri.recipients.toString());
+
+  return formData;
+  // return {
+  //   event_name: veri.eventName,
+  //   event_description: veri.description,
+  //   event_contact_email: veri.organizerEmail,
+  //   event_type: veri.eventDuration,
+  //   event_start_date: new Date(),
+  //   event_end_date: new Date(),
+  //   artwork_name: veri.artworkFile?.name,
+  //   artwork_description: veri.description,
+  //   artwork_file: veri.artworkFile,
+  //   live_distribution: veri.distributionMethod === 'QR-code' ? true : false,
+  //   live_distribution_url: '/test',
+  //   live_distribution_password: 'test',
+  //   status: veri.status,
+  //   recipients: veri.recipients,
+  // };
 };
