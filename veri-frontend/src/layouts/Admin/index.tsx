69b8e2d9e9de03cb98b8d797c5d376da84e77334
shmodarresi <sh.modarresy@gmail.com>
2022-12-20 13:47:04 +0100

    fix: merge bugs fixed

--- veri-frontend/src/layouts/Admin/index.tsx
+++ veri-frontend/src/layouts/Admin/index.tsx
@@ -2,18 +2,19 @@ import { Box, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { ChakraProvider } from '@chakra-ui/provider';
 import { MdViewList } from 'react-icons/md';
 import { HiUser } from 'react-icons/hi2';
-import { matchPath, Outlet, useMatch } from 'react-router-dom';
+import { Outlet, useMatch } from 'react-router-dom';
 import useAuth from '../../contexts/useAuth';
 import { SidebarLinkProps } from '../../design-system/atoms/SidebarLink';
 import { Sidebar } from '../../design-system/organisms/Sidebar';
 import adminTheme from '../../design-system/theme/adminTheme';
 import { useEffect } from 'react';
+import { ADMIN_URL } from '../../Global';
 
 export const DashboardLayout = () => {
   const { setColorMode } = useColorMode();
   const { logout, user } = useAuth();
-  const veriMatch = useMatch({ path: '/admin/veri', end: false });
-  const recipientsMatch = useMatch({ path: '/admin/send', end: false });
+  const veriMatch = useMatch({ path: `${ADMIN_URL}/veri`, end: false });
+  const recipientsMatch = useMatch({ path: `${ADMIN_URL}/send`, end: false });
   const logo = 'VERI Admin';
 
   const adminRoutes: SidebarLinkProps[] = [
