05ed67dc9eed5d8b05f649e67a0ce5d626e98604
JPruefer <115001804+JPruefer@users.noreply.github.com>
2023-01-24 09:58:23 +0100

    Scanner without auth (#79)
    * Make scanner available without authentication
    
    * add correct yarn lock
    
    * small fixes

---
+++ veri-backend/src/middlewares/admin.middleware.ts
@@ -0,0 +1,43 @@
+import { RequestHandler, Response } from 'express';
+import { CustomRequest } from '@interfaces/request.interface';
+import { ADMIN_USER } from '@/constants/admin';
+import { Users } from '@models/users.model';
+import { HttpException } from '@exceptions/HttpException';
+
+const adminMiddleware = (): RequestHandler => {
+  return (req: CustomRequest, res: Response, next) => {
+    Users.query()
+      .select()
+      .from('users')
+      .where('email', '=', ADMIN_USER.email)
+      .first()
+      .then((user) => {
+        const userId = user?.id;
+        if (userId === undefined) {
+          next(
+            new HttpException(
+              500,
+              `Internal server error: Could not find admin user`
+            )
+          );
+        }
+
+        req.user = {
+          id: Number(userId),
+          email: ADMIN_USER.email,
+          password: ADMIN_USER.password,
+        };
+        next();
+      })
+      .catch(() => {
+        next(
+          new HttpException(
+            500,
+            `Internal server error: Could not find admin user`
+          )
+        );
+      });
+  };
+};
+
+export default adminMiddleware;
