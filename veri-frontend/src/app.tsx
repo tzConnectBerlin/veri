65e71f44a4d40dad65c7b116c1645f07624f790a
shmodarresi <sh.modarresy@gmail.com>
2023-01-25 17:55:50 +0100

    style(theme): change initial color

--- veri-frontend/src/app.tsx
+++ veri-frontend/src/app.tsx
@@ -1,8 +1,4 @@
-import {
-  ChakraProvider,
-  ColorModeScript,
-  useColorMode,
-} from '@chakra-ui/react';
+import { ChakraProvider, useColorMode } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router';
 import { AuthProvider } from './contexts/useAuth';
@@ -20,7 +16,6 @@ function App() {
   return (
     <Suspense fallback={'...Loading'}>
       <ChakraProvider theme={adminTheme}>
-        <ColorModeScript initialColorMode="light" />
         <ToastProvider placement="bottom-right" autoDismiss>
           <Fonts />
           <BrowserRouter>
